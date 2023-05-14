049fb7981e5b08932e897787736c699ade38bc99
Mihail Mihaylov <tielass@gmail.com>
2023-05-14 16:36:33 +0200

    task breakdown

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const fdKey = 'ZKfdJ0k8bhRoXpu7k4AgTA';
+require('dotenv').config();
+
+// 1. Create a command line program in a language of your choice, which retrieves the information of  a GitHub User
+// 1.1 Your program should be able to get GitHub user login (username) and Freshdesk  subdomain from the command line.
+// 1.2 For authentication assume GitHub personal access token is given in GITHUB_TOKEN  environmental variable and Freshdesk API key is given in FRESHDESK_TOKEN  environmental variable.
+
+
+
+// 2. Create a new Contact or updates an existing contact in Freshdesk, using their  respective APIs with the information you fetched from GitHub
+
+// 3. Please provide unit tests for the main program functionality. Create a separate module  for the unit tests.
+
+// 4. Please provide a README.md file with instructions on how to run the program and the  tests.
+
+// Optional: Persist login, name, and the creation date of the GitHub user in a table in a  relational database of your choice. If you choose to work on this optional task, please  include the create script of your database as a file in your repo.
+
+// process.env.GITHUB_TOKEN
+// process.env.FRESHDESK_TOKEN
